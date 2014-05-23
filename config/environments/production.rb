ffc1769442058e9b115b30114fe5fc72e9750045
adrails <adrails123@gmail.com>
2014-05-22 23:37:57 -0700

    changing config/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Vms::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -20,7 +20,7 @@ Vms::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'http://adrails-vms.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   enable_starttls_auto: true,
